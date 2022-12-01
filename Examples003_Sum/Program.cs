7eb55c4a7cd05b05ee9580b3eae3f80f22d9487b
Vamik76 <merts_v@mail.ru>
2022-12-02 02:26:57 +1000

    Random

--- Examples003_Sum/Program.cs
+++ Examples003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA =31;
-int namberB =5;
+﻿int numberA = new Random().Next(1,10); // 1,2,3,4 .... 9
+Console.WriteLine(numberA);
+int namberB = new Random().Next(1,10);
+Console.WriteLine(namberB);
 int res=numberA+namberB;
 Console.WriteLine(res);
